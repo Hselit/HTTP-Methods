9690cde4f310893409d6f1d892f7bfde3e70916f
Hselit <tileshkathiresan@gmail.com>
2025-03-17 12:03:33 +0530

    added try/catch

--- routes/users.js
+++ routes/users.js
@@ -1,52 +1,68 @@
 var express = require('express');
 var router = express.Router();
-const db = require('../mysql');
+const db = require('../config/mysql');
+const bcrypt = require('bcrypt');
 
 const app = express();
 app.use(express.json());
 
 //get method
-router.get('/', function(req, res, next) {
-  console.log("inside user route");
-  db.query('select * from employee',(err,values)=>{
-    if(err){
-      res.status(500).send("Error Occured.."+err);
-    } else{
-      console.log(values);
-      res.json(values);
-    }
-  })
+router.get('/', function(req, res) {
+  try{
+    db.query('select * from employee',(err,values)=>{
+      if(err){
+        res.status(500).send("Error Occured.."+err);
+      } else{
+        console.log(values);
+        res.json(values);
+      }
+    });  
+  }
+  catch(error){
+    res.status(500).json({"message":"Internal Server Error "+error});
+  }
 });
 
+
 //get by id
-router.get('/:id',function(req,res,next){
-  const userid = req.params.id;
+router.get('/:id',function(req,res){
+  try{
+    const userid = req.params.id;
   console.log(userid);
+  debugger;
 
   db.query('select * from employee where id = ?',[userid],(err,value)=>{
     if(err){
       res.status(500).send("Error Occured :"+err);
     } else {
       if(value.length > 0){
-        res.json(value[0]);
-      } else{
-        res.status(400).send("User Not Found..");
-      }
+        return res.json(value[0]);
+      } 
+      return res.status(400).send("User Not Found..");
+      
     }
   });
+  }
+  catch(error){
+    res.status(500).json({"message":"Error Occured "+error});
+  }
+  
 });
 
+
 //post method
-router.post('/',function(req,res,next){
-  console.log(req.body);
-  const {name,age,role} = req.body;
-  console.log(name);
-  if(!name || !age || !role){
+router.post('/',async function(req,res){
+  try{
+    const {name,age,role,password} = req.body;
+
+  if(!name || !age || !role || !password){
     return res.status(400).json({message:"All fields are required",type:"error"});
   }
+  let salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password,salt);
 
-  const sql = 'INSERT INTO employee (name,age,role) VALUES (?,?,?);';
-  const value = [name,age,role];
+  const sql = 'INSERT INTO employee (name,age,role,password) VALUES (?,?,?,?);';
+  const value = [name,age,role,hashedPassword];
   db.query(sql,value,(err,result)=>{
     if(err){
       res.status(500).send("Error Occured: "+err);
@@ -54,41 +70,59 @@ router.post('/',function(req,res,next){
       res.status(201).json({"message":"User Created","id":result.insertId});
     }
   });
-
+  }
+  catch(error){
+    res.status(500).json({"message":"Error Occured "+error})
+  }
 });
 
-//put method
-router.put('/:id',function(req,res,next){
-  const empid = req.params.id;
-  const {name,age,role} = req.body;
 
-  const sql = "update employee set name = ?, age = ?, role = ? where id = ?";
+//put method
+router.put('/:id',function(req,res){
+  try{
+    const empid = req.params.id;
+  const {name,age,role,password} = req.body;
+
+  if(!name && !age && !role && !password){
+    return res.status(400).json({message:"Atleast one field required.."});
+  }
+
+  const sql = "update employee set name = ?, age = ?, role = ?,password = ? where id = ?";
   const searchsql = "select id from employee where id = ?";
-  db.query(searchsql,[empid],(err,value)=>{
+  db.query(searchsql,[empid],async (err,value)=>{
      if(err){
       return res.status(400).json({message:"No Employee found with this Id :"+empid});
      } 
      if(value.length > 0){
-      const v = [name,age,role,empid];
+      let salt = await bcrypt.genSalt(10);
+      let hashedpass = await bcrypt.hash(salt,password);
+      const v = [name,age,role,hashedpass,empid];
       db.query(sql,v,(err,result)=>{
         if(err){
           res.status(500).json({message:"error occured :"+err});
         }else if(result.affectedRows === 0){
-          res.status(400).json({message:"Employee Not Found"});
+          res.status(400).json({"message":"Employee Not Found"});
         } else{
-          res.status(200).json({message:"Employee Details Updated.."});
+          res.status(200).json({"message":"Employee Details Updated.."});
         }
       });
      }
      else{
-      return res.status(400).json({message:"No Employee found with this Id :"+empid});
+      return res.status(400).json({"message":"No Employee found with this Id :"+empid});
      }
     });
+  }
+  catch(error){
+    res.status(500).json({"message":"Error Occured "+error});
+  }
 });
 
-router.patch('/:id',function(req,res,next){
-  const id = req.params.id;
-  const {name,age,role} = req.body;
+
+//patch method
+router.patch('/:id',function(req,res){
+  try{
+    const id = req.params.id;
+  const {name,age,role,password} = req.body;
 
   if(!name && !age && !role){
     return res.status(400).json({message:"Atleast one field required.."});
@@ -119,12 +153,18 @@ router.patch('/:id',function(req,res,next){
       res.status(200).json({message:"Data Updated.."});
     }
   });
+  }
+  catch(error){
+    res.status(500).json({"message":"Error Occured:"+error});
+  }
+  
 });
 
 
 //delete method
-router.delete('/:id',function(req,res,next){
-  const empid = req.params.id;
+router.delete('/:id',function(req,res){
+  try{
+    const empid = req.params.id;
   const sql = "delete from employee where id = ?"
 
   db.query(sql,[empid],(err,result)=>{
@@ -136,6 +176,11 @@ router.delete('/:id',function(req,res,next){
       res.status(200).json({message:"Details Deleted"});
     }
   });
+  }
+  catch(error){
+    res.status(500).json({"message":"Error Occured"+error});
+  }
+  
 });
 
 
